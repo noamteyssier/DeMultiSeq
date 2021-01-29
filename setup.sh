1b3fa602fedd8ab8c7380463dde67c0ff68b5321
noamteyssier <nb.teyssier@gmail.com>
2021-01-29 11:04:47 -0800

    add setup script

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# installs cargo if not found
+if [ ! hash cargo ]; then
+  curl https://sh.rustup.rs -sSf | sh
+fi
+
+# build bin directory
+if [ ! -d "bin/"]; then
+  mkdir bin/
+fi
+
+# make optimized binary
+cargo build --release
+
+# symlink binary to bin
+ln -s $(pwd)/target/release/demux bin/demux
